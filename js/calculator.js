e9f20b92db0f147bda9cb2532db8cf9e4c6573f8
Fernando Briano <fernando@picandocodigo.net>
2019-05-29 22:48:37 +0100

    Update JS to listen to amount change and trigger discount

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,7 @@
 var amountInput = document.getElementById("amount");
 var discounted = document.getElementById("discounted");
 
+// Declare discounts
 var efp5 = {
   element: document.getElementById("efp5"),
   discount: 5
@@ -21,9 +22,9 @@ var tattoo = {
   element: document.getElementById("tattoo"),
   discount: 20
 };
-
 var discounts = [efp5, efp10, daytime, ambassador, tattoo];
 
+// Specific "uncheck" other discounts that don't apply
 efp5.element.addEventListener('change', function(){
   if (efp10.element.checked == true){
     efp10.element.checked = false;
@@ -36,8 +37,8 @@ efp10.element.addEventListener('change', function(){
   }
 })
 
+
 tattoo.element.addEventListener('change', function(){
-  // Doesn't stack up with any discount, except EFP %
   [daytime, ambassador].forEach(function(disc){
     if (disc.element.checked == true) {
       disc.element.checked = false;
@@ -53,15 +54,21 @@ tattoo.element.addEventListener('change', function(){
   });
 })
 
-
+// Calculate discounts every time we check something
 discounts.forEach(function(disc){
   disc.element.addEventListener('change', function(){
     calculate_discounts();
   });
 });
 
+// Calculate discounts every time we check something
+amountInput.addEventListener('input', function(){
+  calculate_discounts();
+})
+
 function calculate_discounts(){
   if(amountInput.value == '' || amountInput.value == null){
+    discounted.value = '';
     return;
   }
   var amount = amountInput.value;
