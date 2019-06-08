a9940b59074ae655596f1636f8fa36e74ea6365a
Fernando Briano <fernando@picandocodigo.net>
2019-06-08 14:49:34 +0100

    Revert "Tattoo and Ambassador DO stack up", they don't This reverts commit 30557b6f3f41dd9c3745415c8029352c85dbf56b.

--- js/calculator.js
+++ js/calculator.js
@@ -37,10 +37,16 @@ efp10.element.addEventListener('change', function(){
 })
 
 tattoo.element.addEventListener('change', function(){
+  if (ambassador.element.checked == true) {
+    ambassador.element.checked = false;
+  }
   refreshUI(this);
 });
 
 ambassador.element.addEventListener('change', function(){
+  if(tattoo.element.checked == true){
+    tattoo.element.checked = false;
+  }
   refreshUI(ambassador.element);
 });
 
@@ -59,6 +65,7 @@ amountInput.addEventListener('input', function(){
 dtAmountInput.addEventListener('input', function(){
   calculate_discounts();
 })
+
 
 function calculate_discounts(){
   if (can_calculate_dt() || can_calculate_discount()){
