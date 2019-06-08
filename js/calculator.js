0a418853b4ff0eff14b62b9e8b12ce695731a1b0
Fernando Briano <fernando@picandocodigo.net>
2019-06-08 14:48:17 +0100

    Adds double voucher discounts

--- js/calculator.js
+++ js/calculator.js
@@ -19,36 +19,46 @@ var tattoo = {
   element: document.getElementById("tattoo"),
   discount: 20
 };
+var doubled = document.getElementById("doubled");
 var discounts = [efp5, efp10, ambassador, tattoo];
 
-// Specific "uncheck" other discounts that don't apply
+
+// Exclude/uncheck discounts that don't apply
 efp5.element.addEventListener('change', function(){
   if (efp10.element.checked == true){
     efp10.element.checked = false;
   }
-  refreshUI(efp10.element);
+  refreshUI();
 });
 
 efp10.element.addEventListener('change', function(){
   if (efp5.element.checked == true){
     efp5.element.checked = false;
   }
-  refreshUI(efp5.element);
+  refreshUI();
 })
+// END: Exclude/uncheck discounts that don't apply
 
 tattoo.element.addEventListener('change', function(){
-  refreshUI(this);
+  refreshUI();
 });
 
 ambassador.element.addEventListener('change', function(){
-  refreshUI(ambassador.element);
+  refreshUI();
 });
+
+doubled.addEventListener('change', function(){
+  refreshUI();
+})
 
 // Calculate discounts every time we check something
 discounts.forEach(function(disc){
   disc.element.addEventListener('change', function(){
     calculate_discounts();
   });
+});
+doubled.addEventListener('change', function(){
+  calculate_discounts();
 });
 
 // Calculate discounts every time we check something
@@ -70,6 +80,7 @@ function discount_amount(){
   if(!can_calculate_discount()){
     return 0;
   }
+  refreshDouble();
   var totalDiscount = parseFloat(amountInput.value);
   discounts.forEach(function(discount){
     if (discount.element.checked == true){
@@ -83,6 +94,7 @@ function dt_discount_amount(){
   if(!can_calculate_dt()){
     return 0;
   }
+  refreshDouble();
   var amount = parseFloat(dtAmountInput.value);
   discounts.forEach(function(discount){
     if (discount.element.checked == true){
@@ -105,7 +117,7 @@ function can_calculate_discount(){
 }
 
 // Styling:
-function refreshUI(elem){
+function refreshUI(){
   discounts.forEach(function(discount){
     elem = discount.element;
     if(elem.checked == true){
@@ -119,5 +131,17 @@ function refreshUI(elem){
     dtAmountInput.disabled = true;
   } else {
     dtAmountInput.disabled = false;
+  }
+}
+
+function refreshDouble(){
+  if(doubled.checked == true){
+    efp5.discount = 10;
+    efp10.discount = 20;
+    doubled.parentNode.parentNode.classList.add('checked');
+  } else {
+    efp5.discount = 5;
+    efp10.discount = 10;
+    doubled.parentNode.parentNode.classList.remove('checked');
   }
 }
