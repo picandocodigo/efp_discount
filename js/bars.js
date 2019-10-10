564f3c582224c821cad771f46bbbedefeb51b0d8
Fernando Briano <fernando@picandocodigo.net>
2019-10-10 22:17:36 +0100

    Add bookmarklet for bar pages

---
+++ js/bars.js
@@ -0,0 +1,33 @@
+require(
+  ['jquery'],
+  function ($) {
+    function displayRecords(lim, off) {
+      $.ajax({
+        type: "GET",
+        async: true,
+        url: "https://www.brewdog.com/uk/bars/index/load/",
+        data: "limit=" + lim + "&p=" + off + "&category=" + "uk" +
+          "" +
+          "&lat=0&lng=0",
+        cache: false,
+        success: function (result) {
+          result = JSON.parse(result);
+          var html ='';
+          for (var key in result) {
+            if (result.hasOwnProperty(key)) {
+              html += '<a href="' + result[key].url + '">';
+              html += '<div class="tile" style="background-image: url(' + result[key].image + ')">';
+              html += '<div class="textWrapper">';
+              html += '<h5>' + result[key].title + '</h5>';
+              html += '</div>';
+              html += '</div>';
+              html += '</a>';
+            }
+          }
+          $(".imageGrid").html(html);
+        }
+      });
+    }
+    displayRecords(1000, 0);
+  }
+);
