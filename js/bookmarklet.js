9daa8ca202e2286ec683064661ed3c6cf4614106
Fernando Briano <fernando@picandocodigo.net>
2020-09-23 23:00:09 +0100

    Fix bookmarklet JS

--- js/bookmarklet.js
+++ js/bookmarklet.js
@@ -1,9 +1,12 @@
 var discount = localStorage.getItem('brewdog_shop_discount');
+
 if (!discount || discount == 1){
-  discount = (100 - Number(window.prompt("How much discount percent do you get? (10/15/20)", 10))) / 100;
+  discount = (100 - Number(window.prompt("How much discount percent do you get? (10/5)", 10))) / 100;
   localStorage.setItem('brewdog_shop_discount', discount);
 }
-var prices = document.getElementsByClassName("price");
+
+var prices = document.getElementsByClassName("productTile__price");
+
 for(var i = 0; i < prices.length; i++){
   price = parseFloat(prices[i].innerHTML.replace("£", ""));
   prices[i].innerHTML = "£" + Number(price * parseFloat(discount)).toFixed(2);
